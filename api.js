4c7bd2840e4fd1a631134d323099a42bc65a8715
Oleg Plichko <oleg_plichko@epam.com>
2022-09-03 19:14:40 +0300

    skip collect module

--- api.js
+++ api.js
@@ -202,4 +202,130 @@ export const getPublicationsById = `
             }
         }
     }
+
+    fragment MediaFields on Media {
+        url
+        mimeType
+    }
+
+    fragment ProfileFields on Profile {
+        id
+        name
+        bio
+        attributes {
+            displayType
+            traitType
+            key
+            value
+        }
+        isFollowedByMe
+        isFollowing(who: null)
+        followNftAddress
+        metadata
+        isDefault
+        handle
+        picture {
+            ... on NftImage {
+                contractAddress
+                tokenId
+                uri
+                verified
+            }
+            ... on MediaSet {
+                original {
+                    ...MediaFields
+                }
+            }
+        }
+        coverPicture {
+            ... on NftImage {
+                contractAddress {
+                    contractAddress
+                    tokenId
+                    uri
+                    verified
+                }
+            }
+            ... on MediaSet {
+                original {
+                    ...MediaFields
+                }
+            }
+        }
+        ownedBy
+        dispatcher {
+            address
+        }
+        stats {
+            totalFollowers
+            totalFollowing
+            totalPosts
+            totalComments
+            totalMirrors
+            totalPublications
+            totalCollects
+        }
+        followModule {
+            ... on FeeFollowModuleSettings {
+                type
+                amount {
+                    asset {
+                        name
+                        symbol
+                        decimals
+                        address
+                    }
+                    value
+                }
+                recipient
+            }
+            ... on ProfileFollowModuleSettings {
+                type
+            }
+            ... on RevertFollowModuleSettings {
+                type
+            }
+        }
+    }
+
+    fragment PublicationStatsFields on PublicationStats {
+        totalAmountOfMirrors
+        totalAmountOfCollects
+        totalAmountOfComments
+    }
+
+    fragment MetadataOutputFields on MetadataOutput {
+        name
+        description
+        content
+        media {
+            original {
+            ...MediaFields
+            }
+        }
+        attributes {
+            displayType
+            traitType
+            value
+        }
+    }
+
+    fragment Erc20Fields on Erc20 {
+        name
+        symbol
+        decimals
+        address
+    }
+
+    fragment CommentMirrorOfFields on Comment {
+        ...CommentBaseFields
+        mainPost {
+            ... on Post {
+                ...PostFields
+            }
+            ... on Mirror {
+                ...MirrorBaseFields
+            }
+        }
+    }
 `
